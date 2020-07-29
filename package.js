3a60f3f3964ee15502167a6965b1b960117f409b
filipenevola <filipenevola@gmail.com>
2020-07-29 13:01:44 -0400

    Meteor 1.8.2 version

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'quave:settings',
-  version: '1.0.0',
+  version: '0.0.1',
   summary: 'Utility package to get settings for a package',
   git: 'https://github.com/quavedev/settings',
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.10.2');
+  api.versionsFrom('1.8.2');
   api.use('ecmascript');
 
   api.mainModule('settings.js');
